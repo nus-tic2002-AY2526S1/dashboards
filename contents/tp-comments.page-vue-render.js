ebd027689be448038bd68de7d8bdcc7cd71e5599
damithc <1673303+damithc@users.noreply.github.com>
2025-01-04 18:35:44 +0800

    Site Update. [skip ci]

---
+++ contents/tp-comments.page-vue-render.js
@@ -0,0 +1,16 @@
+
+    var pageVueRenderFn = function anonymous(
+) {
+with(this){return _c('div',{attrs:{"id":"app"}},[_c('header',{attrs:{"fixed":""}},[_c('navbar',{attrs:{"type":"dark"},scopedSlots:_u([{key:"brand",fn:function(){return [_c('a',{staticClass:"navbar-brand",attrs:{"href":"https://nus-tic2002-ay2526s1.github.io/website","title":"Home"}},[_c('span',{staticClass:"badge rounded-pill bg-warning text-dark }}"},[_c('span',[_c('span',{staticClass:"fas fa-chevron-circle-left",attrs:{"aria-hidden":"true"}})]),_v(" Go to TIC2002 main site")])])]},proxy:true}])},[_v(" "),_c('li',[_c('a',{staticClass:"nav-link",attrs:{"href":"/dashboards/index.html"}},[_c('span',[_v("Dashboards home")])])]),_v(" "),_c('li',{attrs:{"tags":"m--cs2103 m--cs2113 m--tic2002 m--tic4001 m--tic4002"}},[_c('a',{staticClass:"nav-link",attrs:{"href":"/dashboards/contents/participation.html"}},[_c('span',[_c('strong',[_v("Participation dashboard")])])])]),_v(" "),_c('li',{attrs:{"tags":"m--tic2002"}},[_c('a',{staticClass:"nav-link",attrs:{"href":"https://nus-tic2002-ay2526s1.github.io/ip-dashboard/?search=&sort=groupTitle&sortWithin=title&timeframe=commit&mergegroup=&groupSelect=groupByRepos&breakdown=true&checkedFileTypes=java~md~fxml~sh~bat~gradle~txt"}},[_c('span',[_c('strong',[_v("Project dashboard")])])])])])],1),_v(" "),_c('div',{attrs:{"id":"flex-body"}},[_c('div',{staticClass:"fixed-header-padding",attrs:{"id":"content-wrapper"}},[_m(0),_v(" "),_c('p',[_v("Sorted based on the number of review comments given to "),_c('em',[_v("others'")]),_v(" PRs, but also showing comments on own PRs and other comments given. "),_c('strong',[_v("Updated weekly")]),_v(". Only those who gave at least one "),_c('tooltip',{scopedSlots:_u([{key:"content",fn:function(){return [_v("comments added to a specific place in the PR code")]},proxy:true}])},[_v("review comment")]),_v(" are listed in this dashboard.")],1),_v(" "),_m(1)]),_v(" "),_c('overlay-source',{staticClass:"fixed-header-padding",attrs:{"id":"page-nav","tag-name":"nav","to":"page-nav"}},[_c('div',{staticClass:"nav-component slim-scroll"},[_c('overlay-source',{staticClass:"nav nav-pills flex-column my-0 small no-flex-wrap",attrs:{"id":"mb-page-nav","tag-name":"nav","to":"mb-page-nav"}},[_c('a',{pre:true,attrs:{"class":"nav-link py-1","href":"#tp-comments-dashboard"}},[_v("tP comments dashboard‎")])])],1)]),_v(" "),_c('scroll-top-button')],1),_v(" "),_m(2)])}
+};
+    var pageVueStaticRenderFns = [function anonymous(
+) {
+with(this){return _c('h1',{staticClass:"display-4",attrs:{"id":"tp-comments-dashboard"}},[_v("tP comments dashboard"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#tp-comments-dashboard","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('div',[_c('p',[_c('span',{staticClass:"dimmed"},[_v("[We have not started updating this dashboard yet]")])])])}
+},function anonymous(
+) {
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v(", generated on Sat, 4 Jan 2025, 18:35:39 GMT+8]")])])])}
+}];
+  
